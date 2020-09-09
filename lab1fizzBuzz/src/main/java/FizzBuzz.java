84e960947cc1ef030379f1d43840219fe7ac8f22
jesubellido <jesubellido@gmail.com>
2020-09-08 23:49:11 -0500

    init

---
+++ lab1fizzBuzz/src/main/java/FizzBuzz.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public  static List<String> calculate(int n){
+        List<String> list = new ArrayList<String>();
+        //TODO add your code here
+        return list;
+    }
+}
